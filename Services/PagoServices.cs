7a71d8c8de2fea8bde7e586dc0e90cc31511ee80
devLv14 <luissabando14@gmail.com>
2025-07-02 13:32:52 -0500

    clases servicio faltantes

---
+++ Services/PagoServices.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TiendaReparacion.Data.Entities;
+using TiendaReparacion.Repositories;
+
+namespace TiendaReparacion.Services
+{
+    public interface IPagoServices
+    {
+        Task<List<Pago>> GetByPaymentMethod(MetodoPago metodo);
+        Task<List<Pago>> GetByDateRange(DateTime startDate, DateTime endDate);
+        Task<Pago?> GetById(int id);
+        Task<List<Pago>> GetAll();
+        Task<int> Insert(Pago pago);
+        Task<int> Update(Pago pago);
+        Task<int> Delete(int id);
+    }
+
+    public class PagoServices : IPagoServices
+    {
+        private readonly IPagoRepository _pagoRepository;
+
+        public PagoServices(IPagoRepository pagoRepository)
+        {
+            _pagoRepository = pagoRepository;
+        }
+
+        public async Task<List<Pago>> GetAll()
+        {
+            try
+            {
+                return await _pagoRepository.GetAll();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<Pago?> GetById(int id)
+        {
+            try
+            {
+                return await _pagoRepository.GetById(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<Pago>> GetByPaymentMethod(MetodoPago metodo)
+        {
+            try
+            {
+                return await _pagoRepository.GetByPaymentMethod(metodo);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<Pago>> GetByDateRange(DateTime startDate, DateTime endDate)
+        {
+            try
+            {
+                return await _pagoRepository.GetByDateRange(startDate, endDate);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<int> Insert(Pago pago)
+        {
+            int result = 0;
+            try
+            {
+                await _pagoRepository.Insert(pago);
+                result = await _pagoRepository.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return result;
+        }
+
+        public async Task<int> Update(Pago pago)
+        {
+            int result = 0;
+            try
+            {
+                _pagoRepository.Update(pago);
+                result = await _pagoRepository.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return result;
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            int result = 0;
+            try
+            {
+                var pago = await _pagoRepository.GetById(id);
+                if (pago == null)
+                    throw new Exception("Pago no encontrado");
+
+                _pagoRepository.Delete(pago);
+                result = await _pagoRepository.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return result;
+        }
+    }
+}
---
+++ Services/RepuestoUtilizadoServices.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TiendaReparacion.Data.Entities;
+using TiendaReparacion.Repositories;
+
+namespace TiendaReparacion.Services
+{
+    public interface IRepuestoUtilizadoServices
+    {
+        Task<List<RepuestoUtilizado>> GetByOrderId(int orderId);
+        Task<List<RepuestoUtilizado>> GetByDateRange(DateTime startDate, DateTime endDate);
+        Task<RepuestoUtilizado?> GetById(int id);
+        Task<List<RepuestoUtilizado>> GetAll();
+        Task<int> Insert(RepuestoUtilizado repuestoUtilizado);
+        Task<int> Update(RepuestoUtilizado repuestoUtilizado);
+        Task<int> Delete(int id);
+    }
+
+    public class RepuestoUtilizadoServices : IRepuestoUtilizadoServices
+    {
+        private readonly IRepuestoUtilizadoRepository _repuestoUtilizadoRepository;
+
+        public RepuestoUtilizadoServices(IRepuestoUtilizadoRepository repuestoUtilizadoRepository)
+        {
+            _repuestoUtilizadoRepository = repuestoUtilizadoRepository;
+        }
+
+        public async Task<List<RepuestoUtilizado>> GetAll()
+        {
+            try
+            {
+                return await _repuestoUtilizadoRepository.GetAll();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<RepuestoUtilizado?> GetById(int id)
+        {
+            try
+            {
+                return await _repuestoUtilizadoRepository.GetById(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<RepuestoUtilizado>> GetByOrderId(int orderId)
+        {
+            try
+            {
+                return await _repuestoUtilizadoRepository.GetByOrderId(orderId);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<RepuestoUtilizado>> GetByDateRange(DateTime startDate, DateTime endDate)
+        {
+            try
+            {
+                return await _repuestoUtilizadoRepository.GetByDateRange(startDate, endDate);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<int> Insert(RepuestoUtilizado repuestoUtilizado)
+        {
+            int result = 0;
+            try
+            {
+                await _repuestoUtilizadoRepository.Insert(repuestoUtilizado);
+                result = await _repuestoUtilizadoRepository.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return result;
+        }
+
+        public async Task<int> Update(RepuestoUtilizado repuestoUtilizado)
+        {
+            int result = 0;
+            try
+            {
+                _repuestoUtilizadoRepository.Update(repuestoUtilizado);
+                result = await _repuestoUtilizadoRepository.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return result;
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            int result = 0;
+            try
+            {
+                var repuestoUtilizado = await _repuestoUtilizadoRepository.GetById(id);
+                if (repuestoUtilizado == null)
+                    throw new Exception("Repuesto utilizado no encontrado");
+
+                _repuestoUtilizadoRepository.Delete(repuestoUtilizado);
+                result = await _repuestoUtilizadoRepository.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return result;
+        }
+    }
+}
